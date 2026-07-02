41dfea3d2eee5ffbe42b9160c7a05562a5450543
nageswari298 <nageswariboddu6@gmail.com>
2026-07-02 23:58:43 +0530

    pat is removed

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: process.env.BASE_URL ,
+    baseURL: process.env.BASE_URL || 'http://localhost:8000',
 
     // Show the browser while learning
     headless: false,
